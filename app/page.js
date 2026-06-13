38aaa276467edf04529fb8300f6bac777fae0232
Kalidash Odekare <shaktiadhikari49@gmail.com>
2026-06-14 01:14:28 +0600

    How it works, why choose us, intraduction, trustsecurity, features sections design completed

--- app/page.js
+++ app/page.js
@@ -1,11 +1,23 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Introduction from "@/components/Introduction";
+import Features from "@/components/Features";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import HowItWorks from "@/components/HowItWorks";
+import TrustSecurity from "@/components/TrustSecurity";
+import PlatformShowcase from "@/components/PlatformShowcase";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Introduction />
+      <Features />
+      <WhyChooseUs />
+      <HowItWorks />
+      <TrustSecurity />
+      <PlatformShowcase />
     </>
   );
 }
