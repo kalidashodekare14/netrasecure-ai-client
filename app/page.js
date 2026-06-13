03c28e1a6310f4c3ec321660b234bc8f086931c1
Kalidash Odekare <shaktiadhikari49@gmail.com>
2026-06-14 01:16:48 +0600

    Chatbot sections design completed to home page

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 import HowItWorks from "@/components/HowItWorks";
 import TrustSecurity from "@/components/TrustSecurity";
 import PlatformShowcase from "@/components/PlatformShowcase";
+import ChatbotSection from "@/components/ChatbotSection";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <HowItWorks />
       <TrustSecurity />
       <PlatformShowcase />
+      <ChatbotSection />
     </>
   );
 }
